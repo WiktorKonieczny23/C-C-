4136b37f6c8b7906203cf6a6899f5c2568bf1c2f
Wiktor Konieczny <135066057+WiktorKonieczny23@users.noreply.github.com>
2024-01-23 19:47:13 +0100

    Add files via upload

---
+++ CpointersW3Zad3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+    int m = 300;
+    float fx = 300.600006;
+    char cht = 'z';
+    printf("Address of m = %p\n", &m);
+    printf("Address of m = %p\n", &fx);
+    printf("Address of m = %p\n", &cht);
+    printf("Value at address of m = %d\n", *&m);
+    printf("Value at address of m = %f\n", *&fx);
+    printf("Value at address of m = %c\n", *&cht);
+    int *pt1;
+    float *pt2;
+    char *pt3;
+    pt1 = &m;
+    pt2 = &fx;
+    pt3 = &cht;
+    printf("Address of m = %p\n", pt1);
+    printf("Address of m = %p\n", pt2);
+    printf("Address of m = %p\n", pt3);
+    printf("Value at address of m = %d\n", *pt1);
+    printf("Value at address of m = %f\n", *pt2);
+    printf("Value at address of m = %c\n", *pt3);
+}
